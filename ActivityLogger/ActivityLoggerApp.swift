9ec49ce362b8f57df3cd43dbdc87cdf60554f3de
Josiah <josiah.pilon@gmail.com>
2023-10-20 12:15:59 -0700

    Initial Commit

---
+++ ActivityLogger/ActivityLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityLoggerApp.swift
+//  ActivityLogger
+//
+//  Created by Josiah Pilon on 2023-10-20.
+//
+
+import SwiftUI
+
+@main
+struct ActivityLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
