88533f61cc4e624bafcf6fdac64c6bf083915d1b
David Wasserman <davidw@grandconsult.com>
2023-12-05 17:00:26 -0500

    Add activity list.

---
+++ ActivityLogger/Views/ActivityListView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityListView.swift
+//  ActivityLogger
+//
+//  Created by David Wasserman on 2023-12-05.
+//
+
+import SwiftUI
+
+struct ActivityListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityListView()
+}
