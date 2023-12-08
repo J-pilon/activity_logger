012eb97dc2dfe0c4de4dc72c752d0c0bb52d96d0
Josiah <josiah.pilon@gmail.com>
2023-12-08 14:12:50 -0800

    Adds @query

--- ActivityLogger/Views/StartActivityView.swift
+++ ActivityLogger/Views/StartActivityView.swift
@@ -8,6 +8,18 @@
 import SwiftUI
 import SwiftData
 
+//@Query(filter: #Predicate<Activity> { activity in
+//  activity.endDate == nil
+//}, sort: \Activity.name) var runningActivity: [Activity]
+
+@Query(filter: #Predicate<Activity> { activity in
+  if activity.endDate == nil {
+      return true
+  } else {
+      return false
+  }
+}) var runningActivity: Activity?
+
 struct StartActivityView: View {
   @State var activity = Activity()
 
@@ -15,23 +27,37 @@ struct StartActivityView: View {
 
     var body: some View {
       VStack {
-        Text("What activity are you starting?")
-          .bold()
-          .font(.title3)
+        if (runningActivity != nil) {
+          Text("35 Minutes")
+        } else {
+          Text("What activity are you starting?")
+            .bold()
+            .font(.title3)
 
-        TextField("Activity", text: $activity.name )
+          TextField("Activity", text: $activity.name)
+            .padding(.leading, 30)
 
-        Button(action: {
-         // activity.name = name
-          print("Activity Name: \(activity.name)")
-          modelContext.insert(activity)
-        }, label: {
-          Text("Start")
-        })
+          Button(action: {
+           // activity.name = name
+            print("Activity Name: \(activity.name)")
+            modelContext.insert(activity)
+          }, label: {
+            Text("Start")
+          })
+        }
       }
     }
 }
 
 #Preview {
-    StartActivityView()
+  do {
+      let config = ModelConfiguration(isStoredInMemoryOnly: true)
+      let container = try ModelContainer(for: Activity.self, configurations: config)
+
+      let example = Activity()
+      return StartActivityView()
+          .modelContainer(container)
+  } catch {
+      fatalError("Failed to create model container.")
+  }
 }
